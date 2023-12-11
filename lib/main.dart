8233b3c929afb42f7dd277240410bd11d16978d6
NikitaGroshev <is911n@icloud.com>
2023-12-11 22:25:00 +0300

    Added products in the horizontal view, product catalog, product card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bar_store/models/Product.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +8,195 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Освежающие напитки',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DrinksPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DrinksPage extends StatelessWidget {
+  final List<Product> drinks = ProductDataProvider().items;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  DrinksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Освежающие напитки'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          const Text(
+            'Больше чем 100 видов напитков',
+            style: TextStyle(color: Colors.black38),
+          ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: drinks.length,
+              itemBuilder: (context, index) => DrinkCard(drink: drinks[index]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: 15),
+          const Text(
+            'Каталог коктейлей',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+            child: ListView.builder(
+              itemCount: drinks.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(drinks[index].title),
+                subtitle: Text('${drinks[index].price} \$.'),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    drinks[index].imgUrl,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                onTap: () {
+                  // Открытие карточки напитка
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          DrinkDetailPage(drink: drinks[index]),
+                    ),
+                  );
+                },
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DrinkCard extends StatelessWidget {
+  final Product drink;
+
+  const DrinkCard({Key? key, required this.drink}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          // Открытие карточки напитка
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DrinkDetailPage(drink: drink),
+            ),
+          );
+        },
+        child: Container(
+          width: 200,
+          margin: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Color(int.parse(drink.color)),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 15),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    15), // Закругление границ для картинки
+                child: Image.network(
+                  drink.imgUrl,
+                  width: 170, // Задайте желаемую ширину для картинки
+                  height: 190, // Задайте желаемую высоту для картинки
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(drink.title, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('${drink.price} \$',
+                      style: const TextStyle(fontSize: 18)),
+                  const AddToCardBatton(),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class DrinkDetailPage extends StatelessWidget {
+  final Product drink;
+
+  const DrinkDetailPage({Key? key, required this.drink}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(drink.title),
+      ),
+      body: Column(
+        children: [
+          Image.network(drink.imgUrl),
+          // Text(drink.title),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                '${drink.price} \$.',
+                style:
+                    const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+              const AddToCardBatton(),
+            ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(drink.description),
+          ),
+          // Добавьте здесь дополнительные детали и кнопки
+        ],
+      ),
+    );
+  }
+}
+
+class AddToCardBatton extends StatelessWidget {
+  const AddToCardBatton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          // Действие при нажатии на кнопку
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green[200], // Фоновый цвет кнопки
+          shape: const CircleBorder(), // Форма круга для кнопки
+        ),
+        child: const Icon(
+          Icons.add,
+          size: 25,
+          color: Colors.white,
+        ));
   }
 }
