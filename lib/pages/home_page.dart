74a47b3c83511880e75fbc6edd8615b6c95c42f1
NikitaGroshev <is911n@icloud.com>
2023-12-11 20:37:49 +0300

    The application was taken from the git and fixed errors preventing the launch. But there is a Provider error at startup.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,72 @@
+import '../models/Product.dart';
+import '../widgets/bottom_bar.dart';
+import '../widgets/catalog.dart';
+import '../widgets/item_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // print('build HomePage');
+
+    // final productData = Provider.of<ProductDataProvider>(context);
+    final productData = context.watch<ProductDataProvider>();
+
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: SafeArea(
+        child: Container(
+            height: MediaQuery.of(context).size.height - 85,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(35),
+                  bottomRight: Radius.circular(35),
+                )),
+            child: ListView(
+              padding: const EdgeInsets.all(10.0),
+              children: <Widget>[
+                Container(
+                  child: const ListTile(
+                    title: Text(
+                      'Освежающие напитки',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('Больше чем 100 видов напитков',
+                        style: TextStyle(
+                          fontSize: 16,
+                        )),
+                    trailing: Icon(Icons.panorama_horizontal),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5.0),
+                  height: 290,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: productData.items.length,
+                    itemBuilder: (context, int index) =>
+                        ChangeNotifierProvider.value(
+                      value: productData.items[index],
+                      child: ItemCard(
+                        key: ValueKey<int>(index),
+                      ),
+                    ),
+
+                    //ItemCard(product: productData.items[index]),
+                  ),
+                ),
+                ...productData.items.map((value) {
+                  return CatalogListTile(imgUrl: value.imgUrl);
+                }).toList(),
+              ],
+            )),
+      ),
+      bottomNavigationBar: BottomBar(),
+    );
+  }
+}
